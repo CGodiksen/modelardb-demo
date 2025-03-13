ad8f1db4a130bcd66f95feee51585611f64ed9de
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-03-13 18:44:27 +0100

    Add prettier and format files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import "@mantine/core/styles.css";
 import "./App.css";
 
-import {createTheme, MantineProvider} from '@mantine/core';
+import { createTheme, MantineProvider } from "@mantine/core";
 
 const theme = createTheme({
-    /** Put your mantine theme override here */
+  /** Put your mantine theme override here */
 });
 
 export default function App() {
-    return (
-        <MantineProvider defaultColorScheme="dark" theme={theme}>
-        </MantineProvider>
-    );
+  return (
+    <MantineProvider defaultColorScheme="dark" theme={theme}></MantineProvider>
+  );
 }
