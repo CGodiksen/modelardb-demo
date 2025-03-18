2412f530f7fdb6508bb5d0292e6dcb98ccb85a5d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-03-18 14:50:38 +0100

    Add Google Maps map to node map component

--- src/components/NodeMap/NodeMap.tsx
+++ src/components/NodeMap/NodeMap.tsx
@@ -1,9 +1,19 @@
-import { Container, Title } from "@mantine/core";
+import { Container } from "@mantine/core";
+import { APIProvider, Map } from "@vis.gl/react-google-maps";
 
 export function NodeMap({}) {
   return (
     <Container fluid>
-      <Title order={2}>Node Map</Title>
+      <APIProvider apiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
+        <Map
+          style={{ height: "72vh" }}
+          defaultCenter={{ lat: 52.0133437, lng: -1.5911737 }}
+          defaultZoom={7}
+          gestureHandling={"greedy"}
+          disableDefaultUI={true}
+          mapTypeId={"hybrid"}
+        />
+      </APIProvider>
     </Container>
   );
 }
