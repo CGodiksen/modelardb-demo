00113da67d4299e8a05c85c309efd900e603c31c
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-03-18 21:01:20 +0100

    Add rust command to get client tables

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,33 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use modelardb_embedded::modelardb::client::{Client, Node};
+use modelardb_embedded::modelardb::ModelarDB;
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+async fn client_tables(url: String) -> Vec<(String, Vec<(String, String)>)> {
+    let node = Node::Server(url);
+    let mut client = Client::connect(node).await.unwrap();
+
+    let table_names = client.tables().await.unwrap();
+
+    let mut tables = vec![];
+    for table_name in table_names {
+        let schema = client.schema(&table_name).await.unwrap();
+
+        let mut columns = vec![];
+        for field in schema.fields() {
+            columns.push((field.name().to_owned(), field.data_type().to_string()));
+        }
+
+        tables.push((table_name, columns));
+    }
+
+    tables
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![client_tables])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
