29c52ae4c98889823c55067df71296b5dc3ae33d
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-03-18 22:26:19 +0100

    Add codemirror SQL editor to query window

--- src/components/QueryEditor/QueryEditor.tsx
+++ src/components/QueryEditor/QueryEditor.tsx
@@ -1,9 +1,22 @@
-import { Container, Title } from "@mantine/core";
+import { Container } from "@mantine/core";
+import CodeMirror from "@uiw/react-codemirror";
+import { useState } from "react";
+import { sql } from "@codemirror/lang-sql";
+import { darcula } from "@uiw/codemirror-theme-darcula";
 
 export function QueryEditor({}) {
+  const [editorText, setEditorText] = useState("");
+
   return (
     <Container fluid>
-      <Title order={4}>Query editor</Title>
+      <CodeMirror
+        value={editorText}
+        height="390px"
+        extensions={[sql()]}
+        onChange={setEditorText}
+        theme={darcula}
+        placeholder={"Enter your SQL query here..."}
+      />
     </Container>
   );
 }
