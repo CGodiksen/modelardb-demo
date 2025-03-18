f4c1a782674a787c418664454c14122f8a218b23
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-03-18 23:48:09 +0100

    Add Tauri command to query from client

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+use arrow::array::RecordBatch;
+use arrow_json::ArrayWriter;
+use datafusion::physical_plan::common;
 use modelardb_embedded::modelardb::client::{Client, Node};
 use modelardb_embedded::modelardb::ModelarDB;
 
@@ -41,11 +44,46 @@ async fn client_tables(url: String) -> Vec<ClientTableResponse> {
     tables
 }
 
+#[derive(serde::Serialize)]
+struct ClientQueryResponse {
+    column_names: Vec<String>,
+    data: Vec<u8>,
+}
+
+#[tauri::command]
+async fn client_query(url: String, query: String) -> ClientQueryResponse {
+    let node = Node::Server(url);
+    let mut client = Client::connect(node).await.unwrap();
+
+    let record_batch_stream = client.read(&query).await.unwrap();
+    let record_batches = common::collect(record_batch_stream).await.unwrap();
+    let record_batch_slice: Vec<&RecordBatch> = record_batches.iter().collect();
+
+    // Write the record batch out as a JSON array.
+    let buf = Vec::new();
+
+    let mut writer = ArrayWriter::new(buf);
+    writer.write_batches(&record_batch_slice).unwrap();
+    writer.finish().unwrap();
+
+    let data = writer.into_inner();
+
+    ClientQueryResponse {
+        column_names: record_batches[0]
+            .schema()
+            .fields()
+            .iter()
+            .map(|field| field.name().to_owned())
+            .collect(),
+        data,
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![client_tables])
+        .invoke_handler(tauri::generate_handler![client_tables, client_query])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
