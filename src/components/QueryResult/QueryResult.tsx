926c8d89953a640c2a8a9e741a9c8483b9a04d76
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-03-19 11:11:40 +0100

    Move table query result into separate component

--- src/components/QueryResult/QueryResult.tsx
+++ src/components/QueryResult/QueryResult.tsx
@@ -1,42 +1,20 @@
-import { ScrollArea, Table } from "@mantine/core";
-import cx from "clsx";
-import { useState } from "react";
-
-import classes from "./QueryResult.module.css";
+import { Card, Text } from "@mantine/core";
+import { QueryResultTable } from "../QueryResultTable/QueryResultTable.tsx";
 
 export function QueryResult({ queryData }: { queryData: any[] }) {
-  const [scrolled, setScrolled] = useState(false);
-
   if (queryData.length === 0) {
-    return <div>No rows returned.</div>;
-  } else {
-    const rows = queryData.map((row, index) => (
-      <Table.Tr key={index}>
-        {Object.values(row).map((value) => (
-          // @ts-ignore
-          <Table.Td>{value}</Table.Td>
-        ))}
-      </Table.Tr>
-    ));
-
     return (
-      <ScrollArea
-        h={300}
-        onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
-      >
-        <Table miw={700}>
-          <Table.Thead
-            className={cx(classes.header, { [classes.scrolled]: scrolled })}
-          >
-            <Table.Tr>
-              {Object.keys(queryData[0]).map((column) => (
-                <Table.Th>{column}</Table.Th>
-              ))}
-            </Table.Tr>
-          </Table.Thead>
-          <Table.Tbody>{rows}</Table.Tbody>
-        </Table>
-      </ScrollArea>
+      <Card shadow="sm" padding={2} radius="md" withBorder h={"100%"}>
+        <Text ps={10} pt={5}>
+          Execute a query to see the results.
+        </Text>
+      </Card>
+    );
+  } else {
+    return (
+      <Card shadow="sm" padding={2} radius="md" withBorder h={"100%"}>
+        <QueryResultTable queryData={queryData}></QueryResultTable>
+      </Card>
     );
   }
 }
---
+++ src/components/QueryResultTable/QueryResultTable.tsx
@@ -0,0 +1,38 @@
+import { ScrollArea, Table } from "@mantine/core";
+import cx from "clsx";
+import { useState } from "react";
+
+import classes from "./QueryResultTable.module.css";
+
+export function QueryResultTable({ queryData }: { queryData: any[] }) {
+  const [scrolled, setScrolled] = useState(false);
+
+  const rows = queryData.map((row, index) => (
+    <Table.Tr key={index}>
+      {Object.values(row).map((value) => (
+        // @ts-ignore
+        <Table.Td>{value}</Table.Td>
+      ))}
+    </Table.Tr>
+  ));
+
+  return (
+    <ScrollArea
+      h={300}
+      onScrollPositionChange={({ y }) => setScrolled(y !== 0)}
+    >
+      <Table miw={700}>
+        <Table.Thead
+          className={cx(classes.header, { [classes.scrolled]: scrolled })}
+        >
+          <Table.Tr>
+            {Object.keys(queryData[0]).map((column) => (
+              <Table.Th>{column}</Table.Th>
+            ))}
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
