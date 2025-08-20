6098496db24b13e70e440f7b403e838501461ff1
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2025-08-20 19:02:32 +0200

    Add type hints to flight server

--- comparison-system/app/main.py
+++ comparison-system/app/main.py
@@ -2,26 +2,29 @@ import os
 
 import pyarrow as pa
 import pyarrow.flight
+from pyarrow._flight import (ServerCallContext, FlightDescriptor, MetadataRecordBatchReader, FlightMetadataWriter,
+                             Ticket, Action)
 
 
 class FlightServer(pa.flight.FlightServerBase):
-    def __init__(self, location, **kwargs):
+    def __init__(self, location: str, **kwargs):
         super(FlightServer, self).__init__(location, **kwargs)
         self._location = location
 
-    def list_flights(self, context, criteria):
+    def list_flights(self, context: ServerCallContext, criteria: bytes):
         raise NotImplementedError("list_flights is not implemented.")
 
-    def get_flight_info(self, context, descriptor):
+    def get_flight_info(self, context: ServerCallContext, descriptor: FlightDescriptor):
         raise NotImplementedError("get_flight_info is not implemented.")
 
-    def do_put(self, context, descriptor, reader, writer):
+    def do_put(self, context: ServerCallContext, descriptor: FlightDescriptor, reader: MetadataRecordBatchReader,
+               writer: FlightMetadataWriter):
         raise NotImplementedError("do_put is not implemented.")
 
-    def do_get(self, context, ticket):
+    def do_get(self, context: ServerCallContext, ticket: Ticket):
         raise NotImplementedError("do_get is not implemented.")
 
-    def do_action(self, context, action):
+    def do_action(self, context: ServerCallContext, action: Action):
         if action.type == "reset_node":
             self.do_reset_node()
         elif action.type == "flush_node":
@@ -29,7 +32,7 @@ class FlightServer(pa.flight.FlightServerBase):
         else:
             raise NotImplementedError(f"Action '{action.type}' is not implemented.")
 
-    def list_actions(self, context):
+    def list_actions(self, context: ServerCallContext):
         return [("reset_node", "Reset the node"), ("flush_node", "Flush the node")]
 
     def do_reset_node(self):
